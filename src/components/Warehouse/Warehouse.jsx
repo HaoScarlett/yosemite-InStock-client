0b2781a2d044b52518aee1e2705a44690ab3d5dd
Newiq <784751939@qq.com>
2024-09-04 11:53:15 -0400

    add the src of img

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Warehouse.scss';
 import { Link, NavLink } from 'react-router-dom';
+import backbtn from '../../assets/Icons/arrow_back-24px.svg';
+import editbtn from '../../assets/Icons/edit-24px.svg';
 
 //Warehouse Details component. This does not include the inventory list below the warehouse details. 
 function Warehouse() {
@@ -8,10 +10,12 @@ function Warehouse() {
     <section className="warehouse">
         <div className="warehouse__header">
             <Link to='/'>
-                <img className='warehouse__header__btn__back' src="" alt="back button" />
+                <img className='warehouse__header__btn__back' src={backbtn} alt="back button" />
             </Link>
             <h1 className='warehouse__header__title'></h1>
-            <img className='warehouse__header__btn__edit' src="" alt="round edit button" />
+            <Link>
+                <img className='warehouse__header__btn__edit' src={editbtn} alt="round edit button" />
+            </Link>
         </div>
         <div className="warehouse__details">
             <div className='warehouse__details__address'></div>
