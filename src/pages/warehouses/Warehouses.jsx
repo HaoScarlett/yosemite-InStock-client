5e7eb39107ab09cda621d84e039d5af00bd6f561
Newiq <784751939@qq.com>
2024-09-05 14:10:51 -0400

    remove test component in warehouse page

--- src/pages/warehouses/Warehouses.jsx
+++ src/pages/warehouses/Warehouses.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Warehouse from '../../components/Warehouse/Warehouse'
 function Warehouses() {
   return (
     <div>
       <h1>Warehouses Page</h1>
-      <Warehouse />
     </div>
   )
 }
