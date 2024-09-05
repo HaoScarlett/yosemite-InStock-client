6eaf3deabfeb385a9527cc3f1524fa199d74ccba
Brianna Dunfield <bdunfield@mta.ca>
2024-09-05 14:40:43 -0300

    Getting and displaying info from API

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -1,45 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './InventoryItem.scss';
 import InOutStock from '../InOutStock/InOutStock.jsx';
 import SectionHeader from '../LowLevelComponents/SectionHeader/SectionHeader.jsx';
+import { fetchInventoryItem } from '../../utils/api.js';
 
-export default function InventoryItem() {
-	const name = 'Television';
-	const itemDescription =
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-	const itemCategory = 'Electronics';
-	const itemQuantity = '500';
-	const itemWarehouse = 'Manhatten';
+export default function InventoryItem({ itemId }) {
+	const [inventoryItem, setInventoryItem] = useState({});
+	if (!itemId) {
+		itemId = 1;
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetchInventoryItem(itemId);
+				setInventoryItem(response.data[0]);
+				console.log(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+
+	const {warehouse_name, item_name, description, category, status, quantity}  = inventoryItem;
+	const inStock = status === 'In Stock' ? true : false;
 
 	return (
 		<section className='inventory'>
 			<div className='inventory-header'>
 				<div className='inventory-header__left'>
-					<SectionHeader text={name} />
+					<SectionHeader text={item_name} />
 				</div>
 				<p>button</p>
 			</div>
 			<div className='inventory-body'>
 				<div className='inventory-info'>
 					<label>ITEM DESCRIPTION:</label>
-					<p className='p2-body-medium'>{itemDescription}</p>
+					<p className='p2-body-medium'>{description}</p>
 					<label>CATEGORY:</label>
-					<p className='p2-body-medium'>{itemCategory}</p>
+					<p className='p2-body-medium'>{category}</p>
 				</div>
 				<div className='inventory-body__right'>
 					<div className='inventory-stats'>
 						<div className='inventory-stats__status-quant'>
 							<div>
 								<label>STATUS:</label>
-								<InOutStock inStock={true} />
+								<InOutStock inStock={inStock} />
 							</div>
 							<div>
 								<label>QUANTITY:</label>
-								<p className='p2-body-medium'>{itemQuantity}</p>
+								<p className='p2-body-medium'>{quantity}</p>
 							</div>
 						</div>
 						<label>WAREHOUSE:</label>
-						<p className='p2-body-medium'>{itemWarehouse}</p>
+						<p className='p2-body-medium'>{warehouse_name}</p>
 					</div>
 				</div>
 			</div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
+console.log('API URL:', import.meta.env.VITE_API_URL);
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export const fetchInventoryList = () => {
-    return axios.get(`${API_URL}/api/inventories`)
-} 
+	return axios.get(`${API_URL}/api/inventories`);
+};
+
+export const fetchInventoryItem = (id) => {
+	return axios.get(`${API_URL}/api/inventories/${id}`);
+};
