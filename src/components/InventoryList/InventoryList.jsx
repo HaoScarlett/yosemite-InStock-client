d64973fe939fa55704c19dfe5cd85befaab262e6
Newiq <784751939@qq.com>
2024-09-06 14:27:03 -0400

    fix warehouse lost problem in inventory list

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,57 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { fetchInventoryList } from '../../utils/api.js'
-import ItemRow from '../ItemRow/ItemRow.jsx'
-import SearchBar from '../LowLevelComponents/SearchBar/SearchBar.jsx'
-import './InventoryList.scss'
+import React, { useEffect, useState } from 'react';
+import { fetchInventoryList } from '../../utils/api.js';
+import ItemRow from '../ItemRow/ItemRow.jsx';
+import SearchBar from '../LowLevelComponents/SearchBar/SearchBar.jsx';
+import './InventoryList.scss';
 
-function InventoryList({id, className}) {
-    const [inventoryList, setInventoryList] = useState([])
+function InventoryList({ id, className }) {
+const [inventoryList, setInventoryList] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetchInventoryList();
-                setInventoryList(response.data)
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchData();
-    }, [id])
+useEffect(() => {
+const fetchData = async () => {
+    try {
+    const response = await fetchInventoryList();
+    setInventoryList(response.data);
+    console.log(response.data);
+    } catch (error) {
+    console.error(error);
+    }
+};
+fetchData();
+}, [id]);
 
-    return (
-        <div className={`inventory-list layout ${className === 'hidden' ? 'no-shadow' : ''}`}>
-        {className !== 'hidden' && (
-        <>
-            <h1 className="inventory-list__title">Inventory</h1>
-            <SearchBar className="inventory-list__search" />
-        </>
-        )}
+const showWarehouse = className !== 'hidden';
 
-        <table className="inventory-table">
-        <thead>
-            <tr className="inventory-header h4-table-header">
-            <th>INVENTORY ITEM</th>
-            <th>CATEGORY</th>
-            <th>STATUS</th>
-            <th>QTY</th>
-            {className !== 'hidden' && <th>WAREHOUSE</th>}
-            <th>ACTIONS</th>
-            </tr>
-        </thead>
-        <tbody>
+return (
+<div className={`inventory-list layout ${!showWarehouse ? 'no-shadow' : ''}`}>
+    {showWarehouse && (
+    <>
+        <h1 className="inventory-list__title">Inventory</h1>
+        <SearchBar className="inventory-list__search" />
+    </>
+    )}
+
+    <table className="inventory-table">
+    <thead>
+        <tr className="inventory-header h4-table-header">
+        <th>INVENTORY ITEM</th>
+        <th>CATEGORY</th>
+        <th>STATUS</th>
+        <th>QTY</th>
+        {showWarehouse && <th>WAREHOUSE</th>}
+        <th>ACTIONS</th>
+        </tr>
+    </thead>
+    <tbody>
         {inventoryList.map((inventory) => (
         <ItemRow 
             key={inventory.id} 
             item={inventory} 
-            showWarehouse={className !== 'hidden'} 
+            showWarehouse={showWarehouse}
         />
         ))}
-        </tbody>
-        </table>
-    </div>
-    );
+    </tbody>
+    </table>
+</div>
+);
 }
 
-export default InventoryList
+export default InventoryList;
--- src/components/ItemRow/ItemRow.jsx
+++ src/components/ItemRow/ItemRow.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import './ItemRow.scss'
-import chevronIcon from '../../assets/Icons/chevron_right-24px.svg'
+import React from 'react';
+import './ItemRow.scss';
+import chevronIcon from '../../assets/Icons/chevron_right-24px.svg';
 import { useMediaQuery } from 'react-responsive';
-import deleteIcon from '../../assets/Icons/delete_outline-24px.svg'
-import editIcon from '../../assets/Icons/edit-24px.svg'
-import InOutStock from '../LowLevelComponents/InOutStock/InOutStock.jsx'
+import deleteIcon from '../../assets/Icons/delete_outline-24px.svg';
+import editIcon from '../../assets/Icons/edit-24px.svg';
+import InOutStock from '../LowLevelComponents/InOutStock/InOutStock.jsx';
 
-function ItemRow({ item }) {
+function ItemRow({ item, showWarehouse }) {
   const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
-  const inStock = item.status === 'In Stock' ? true : false;
+  const inStock = item.status === 'In Stock';
 
   const MobileView = ({ item, showWarehouse }) => {
     return (
       <div className='item-row'>
         <div className="inventory-item">
           <div className='inventory-item__name'>
-            <span data-label="Inventory Item"><a className='item-name h3-links' href="">{item.item_name}
-            </a>
+            <span data-label="INVENTORY ITEM">
+              <a className='item-name h3-links' href="">{item.item_name}</a>
               <img className='icon' src={chevronIcon} alt="" />
             </span>
-            <span data-label="Category">{item.category}</span>
+            <span data-label="CATEGORY">{item.category}</span>
           </div>
           <div className='inventory-item__details'>
-            <span data-label="Status" className='status'>
+            <span data-label="STATUS" className='status'>
               <InOutStock inStock={inStock} />
             </span>
             <span data-label="QTY">{item.quantity}</span>
-            {showWarehouse && <span data-label="Warehouse">{item.warehouse_name}</span>}
-          </div></div>
+            {showWarehouse && <span data-label="WAREHOUSE">{item.warehouse_name}</span>}
+          </div>
+        </div>
         <div className='actions'>
-          <span data-label="Actions" className='actions__wrapper'>
+          <span data-label="ACTIONS" className='actions__wrapper'>
             <button className="delete-btn">
               <img src={deleteIcon} alt="delete button" />
             </button>
@@ -39,42 +40,42 @@ function ItemRow({ item }) {
           </span>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   const DesktopView = ({ item, showWarehouse }) => {
     return (
-      <>
-        <tr className='item-row'>
-          <td className="inventory-item__name">
-            <a className='item-name' href="#">{item.item_name}</a>
-            <img className='icon' src={chevronIcon} alt="chevron icon" />
-          </td>
-          <td className='inventory-item__category'>{item.category}</td>
-          <td className='inventory-item__status status'>
-            <InOutStock inStock={inStock} />
-          </td>
-          <td className='inventory-item__quantity'>{item.quantity}</td>
-          {showWarehouse && <td className='inventory-item__warehouse'>{item.warehouse_name}</td>}
-          <td className='actions'>
-            <button className="delete-btn">
-              <img src={deleteIcon} alt="delete button" />
-            </button>
-            <button className="edit-btn">
-              <img src={editIcon} alt="edit button" />
-            </button>
-          </td>
-        </tr>
-      </>
+      <tr className='item-row'>
+        <td className="inventory-item__name">
+          <a className='item-name' href="#">{item.item_name}</a>
+          <img className='icon' src={chevronIcon} alt="chevron icon" />
+        </td>
+        <td className='inventory-item__category'>{item.category}</td>
+        <td className='inventory-item__status status'>
+          <InOutStock inStock={inStock} />
+        </td>
+        <td className='inventory-item__quantity'>{item.quantity}</td>
+        {showWarehouse && <td className='inventory-item__warehouse'>{item.warehouse_name}</td>}
+        <td className='actions'>
+          <button className="delete-btn">
+            <img src={deleteIcon} alt="delete button" />
+          </button>
+          <button className="edit-btn">
+            <img src={editIcon} alt="edit button" />
+          </button>
+        </td>
+      </tr>
     );
-  }
+  };
 
   return (
     <>
-      {isDesktop ? <DesktopView item={item} /> : <MobileView item={item} />}
+      {isDesktop 
+        ? <DesktopView item={item} showWarehouse={showWarehouse} /> 
+        : <MobileView item={item} showWarehouse={showWarehouse} /> 
+      }
     </>
   );
-
 }
 
-export default ItemRow
+export default ItemRow;
