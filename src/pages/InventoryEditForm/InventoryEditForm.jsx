0950d488d4c3f9c0575b00e5ecf7c04255481f44
Scarlett <hao.sijia04@gmail.com>
2024-09-07 15:39:10 -0400

    feat(api): Set up endpoints to handle GET categories request
    - Implemented API endpoints to retrieve categories data.
    - Successfully handles GET requests for categories.

--- src/pages/InventoryEditForm/InventoryEditForm.jsx
+++ src/pages/InventoryEditForm/InventoryEditForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { fetchInventoryItem, updateInventoryItem } from '../../utils/api';
 import CTAButton from '../../components/LowLevelComponents/CTAButton/CTAButton.jsx'
-
+import SectionHeader from '../../components/LowLevelComponents/SectionHeader/SectionHeader.jsx'
 export default function InventoryEditForm() {
     const [item, setItem] = useState();
     const navigate = useNavigate();
@@ -29,11 +29,39 @@ export default function InventoryEditForm() {
     if (!item) return <div>Loading...</div>;
 
     return (
-        <form action="" onSubmit={handleSubmit}>
+        <>
+            <SectionHeader text={'Edit Inventory Item'} url="/inventory/:id" />
+            <form action="" onSubmit={handleSubmit}>
+                <div className='item-form'>
+                    <h2 className='h2-subheader'>Item Details</h2>
+                    <label htmlFor="item-name">
+                        <h3 className="item-name">
+                            <input type="text"
+                                name='item-name'
+                                id='item-name'
+                                placeholder={item.name}
+                                required
+                                maxLength={50}
+                            />
+                        </h3>
+                    </label>
+                    <label htmlFor="item-description">
+                        <h3 className="item-description">
+                            <textarea name="item-description" id="item-description" cols="30" rows="10"
+                                placeholder={item.description}
+                                required
+                            ></textarea>
+                        </h3>
+                    </label>
+                    <label htmlFor="">
+                        <select name="category" id="category">
+                            
+                        </select>
+                    </label>
+                </div>
+                <CTAButton text={'Cancel'} variant='secondary' onClick={handleCancel} type='button' />
+                <CTAButton text={'Save'} type='submit' />
 
-            <CTAButton text={'Cancel'} variant='secondary' onClick={handleCancel} type='button' />
-            <CTAButton text={'Save'} type='submit' />
-
-        </form>
+            </form></>
     )
 }
