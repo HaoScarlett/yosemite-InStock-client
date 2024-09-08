8640d9549e0db0ce7504ec4eabd0650b72abe1f7
yangliudesigns <yangliudesigns@gmail.com>
2024-09-08 02:51:06 -0400

    Revert "Merge pull request #38 from HaoScarlett/feature-fix-home"
    This reverts commit 9a0e338107719d3e181193aee8b5d2f4276678da, reversing
    changes made to 6be51895f8eabc49886bb86b6589e9fda9fc2900.

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,49 +1,55 @@
-import React from 'react';
+import React from 'react'
 import "./DeleteModal.scss";
 import Modal from "react-modal";
 import closeIcon from "../../assets/Icons/close-24px.svg";
 import CTAButton from "../LowLevelComponents/CTAButton/CTAButton";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+
 
 export default function DeleteModal({ 
     deleteModalInfo, 
     onCancel, 
     onConfirm }) {
 
-    // Modal is open if deleteModalInfo contains data
-    const modalIsOpen = Object.keys(deleteModalInfo).length > 0;
+        const modalIsOpen = Object.keys(deleteModalInfo).length > 0;
 
-    return (
-        <Modal
-            className="delete-modal"
-            overlayClassName="delete-modal__overlay"
-            ariaHideApp={false}
-            isOpen={modalIsOpen}
-            onRequestClose={onCancel}
-        >
-            <button className="delete-modal__close-btn" onClick={onCancel}>
-                <img src={closeIcon} alt="close-icon" />
-            </button>
 
-            <h1 className="delete-modal__title h1-page-header">
-                {deleteModalInfo.title}
-            </h1>
-            <p className="delete-modal__text p1-body-large">
-                {deleteModalInfo.text}
-            </p>
+  return (
 
-            <div className="delete-modal__btn">
-                <CTAButton 
-                    text={'Cancel'} 
-                    variant='secondary' 
-                    onClick={onCancel} 
-                />
+    <Modal
+      className="delete-modal"
+      overlayClassName="delete-modal__overlay"
+      ariaHideApp={false}
+      isOpen={modalIsOpen}
+      onRequestClose={onCancel}
+      >
+      <button className="delete-modal__close-btn" onClick={onCancel}>
+        <img src={closeIcon} alt="close-icon" />
+      </button>
 
-                <CTAButton 
-                    text={'Delete'} 
-                    variant='delete' 
-                    onClick={() => onConfirm(deleteModalInfo.id)}  // Replace handleDelete with onConfirm
-                />
-            </div>
-        </Modal>
-    );
+      <h1 className="delete-modal__title h1-page-header">
+        {deleteModalInfo.title}
+      </h1>
+      <p className="delete-modal__text p1-body-large">
+        {deleteModalInfo.text}
+      </p>
+
+      <div className="delete-modal__btn">
+        <CTAButton 
+        text={'Cancel'} 
+        variant='secondary' 
+        onClick={onCancel} />
+
+        <CTAButton 
+        text={'Delete'} 
+        variant='delete' 
+        onClick={handleDelete} 
+        />
+      </div>
+  </Modal>
+
+  )
 }
